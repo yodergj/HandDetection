e16d4150e8763c1f199452b1bc2c3ec64d817bd9
Gabe Yoder <gyoder@stny.rr.com>
2008-04-09 21:19:29 -0400

    More work on the image class

--- image/Image.cpp
+++ image/Image.cpp
@@ -2,18 +2,103 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX(a,b) ( (a) > (b) ? (a) : (b) )
+
 Image::Image()
 {
   mWidth = 0;
   mHeight = 0;
   mBuffer = NULL;
   mBufferSize = 0;
+  mYIQBuffer = NULL;
+  mYIQAlloc = 0;
 }
 
 Image::~Image()
 {
   if ( mBuffer )
     free(mBuffer);
+  if ( mYIQBuffer )
+    free(mYIQBuffer);
+}
+
+int Image::GetWidth()
+{
+  return mWidth;
+}
+
+int Image::GetHeight()
+{
+  return mHeight;
+}
+
+unsigned char* Image::GetRGBBuffer()
+{
+  return mBuffer;
+}
+
+double* Image::GetYIQBuffer()
+{
+  int x, y;
+  unsigned char* srcPixel;
+  double* destPixel;
+
+  if ( !ResizeBuffer(&mYIQBuffer, &mYIQAlloc) )
+    return NULL;
+
+  srcPixel = mBuffer;
+  destPixel = mYIQBuffer;
+  for (y = 0; y < mHeight; y++)
+  {
+    for (x = 0; x < mWidth; x++)
+    {
+      destPixel[0] =   srcPixel[0] * .299 + srcPixel[1] *  .587 + srcPixel[2] *  .114;
+      destPixel[1] = ((srcPixel[0] * .596 + srcPixel[1] * -.275 + srcPixel[2] * -.321)/.596 + 255)/2;
+      destPixel[2] = ((srcPixel[0] * .212 + srcPixel[1] * -.523 + srcPixel[2] *  .311)/.523 + 255)/2;
+
+      srcPixel += 3;
+      destPixel += 3;
+    }
+  }
+
+  return mYIQBuffer;
+}
+
+double* Image::GetScaledRGBBuffer()
+{
+  int x, y;
+  unsigned char* srcPixel;
+  double* destPixel;
+  double maxVal;
+
+  if ( !ResizeBuffer(&mScaledRGBBuffer, &mScaledRGBAlloc) )
+    return NULL;
+
+  srcPixel = mBuffer;
+  destPixel = mYIQBuffer;
+  for (y = 0; y < mHeight; y++)
+  {
+    for (x = 0; x < mWidth; x++)
+    {
+      maxVal = MAX(srcPixel[0], MAX(srcPixel[1], srcPixel[2]));
+      if ( maxVal == 0 )
+      {
+        destPixel[0] = 1;
+        destPixel[1] = 1;
+        destPixel[2] = 1;
+      }
+      else
+      {
+        destPixel[0] = srcPixel[0] / maxVal;
+        destPixel[1] = srcPixel[0] / maxVal;
+        destPixel[2] = srcPixel[0] / maxVal;
+      }
+      srcPixel += 3;
+      destPixel += 3;
+    }
+  }
+
+  return mYIQBuffer;
 }
 
 bool Image::CopyRGBABuffer(int width, int height, int* buffer, int bufferWidth)
@@ -91,6 +176,27 @@ bool Image::SetSize(int width, int height)
   }
   mWidth = width;
   mHeight = height;
+
+  return true;
+}
+
+bool Image::ResizeBuffer(double** buffer, int* bufferAlloc)
+{
+  int sizeNeeded;
+  double* tmp;
+
+  if ( !buffer || !bufferAlloc )
+    return false;
+
+  sizeNeeded = mWidth * mHeight * 3;
+  if ( sizeNeeded > *bufferAlloc )
+  {
+    tmp = (double *)realloc(*buffer, sizeNeeded);
+    if ( !tmp )
+      return false;
+    *buffer = tmp;
+    *bufferAlloc = sizeNeeded;
+  }
 
   return true;
 }
--- image/Image.h
+++ image/Image.h
@@ -6,14 +6,25 @@ class Image
   public:
     Image();
     ~Image();
+    int GetWidth();
+    int GetHeight();
+    unsigned char* GetRGBBuffer();
+    double* GetYIQBuffer();
+    double* GetScaledRGBBuffer();
     bool CopyRGBABuffer(int width, int height, int* buffer, int bufferWidth);
     bool CopyRGBBuffer(int width, int height, unsigned char* buffer, int bufferWidth);
   private:
     bool SetSize(int width, int height);
+    bool ResizeBuffer(double** buffer, int* bufferAlloc);
+
     int mWidth;
     int mHeight;
     unsigned char* mBuffer;
     int mBufferSize;
+    double* mYIQBuffer;
+    int mYIQAlloc;
+    double* mScaledRGBBuffer;
+    int mScaledRGBAlloc;
 };
 
 #endif
